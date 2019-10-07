fd14220ae6e850218f5724b526e8e321afb97104
songwei <song61069140@gmail.com>
2019-10-07 22:41:54 +0800

    完成第11题

---
+++ program1_100/question_11.py
@@ -0,0 +1,43 @@
+"""
+给定 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+
+说明：你不能倾斜容器，且 n 的值至少为 2。
+
+
+
+图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+
+ 
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/container-with-most-water
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left = 0
+        right = len(height) - 1
+        max_area = 0
+        while left < right:
+            b = right - left
+            if height[left] < height[right]:
+                h = height[left]
+                left += 1
+            else:
+                h = height[right]
+                right -= 1
+            area = b*h
+            if max_area < area:
+                max_area = area
+        return max_area
+
+
+if __name__ == '__main__':
+    solu = Solution()
+    print(solu.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
