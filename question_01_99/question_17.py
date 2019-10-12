cb3e0003e6ffc73c2c6ea0e8edadcce5582fcaa7
songwei <song61069140@gmail.com>
2019-10-12 22:45:22 +0800

    完成问题19

---
+++ question_01_99/question_17.py
@@ -0,0 +1,51 @@
+"""
+
+
+给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+
+给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+
+
+
+示例:
+
+输入："23"
+输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+说明:
+尽管上面的答案是按字典序排列的，但是你可以任意选择答案输出的顺序。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
+"""
+
+
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        m = {
+            '2': list('abc'),
+            '3': list('def'),
+            '4': list('ghi'),
+            '5': list('jkl'),
+            '6': list('mno'),
+            '7': list('pqrs'),
+            '8': list('tuv'),
+            '9': list('wxyz'),
+        }
+        if not digits:
+            return []
+        lst = ['']
+        for i in digits:
+            lst = [x + y for x in lst for y in m[i]]
+        return lst
+
+
+if __name__ == '__main__':
+    solu = Solution()
+    print(solu.letterCombinations("4577"))
